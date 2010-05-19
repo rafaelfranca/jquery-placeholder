6723d41d5073f0ea7d3275bfeacb75915fb0a131
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-05-19 16:48:01 -0300

    Criado o plugin de placeholder

---
+++ jquery.placeholder.js
@@ -0,0 +1,18 @@
+jQuery.fn.extend({
+	placeholder: function (text) {
+		$(this).val(text).css('color','#aaa')
+		.focus(function(){
+			if($(this).val() == text){
+				$(this).css('color','').val('');
+			}
+		})
+		.blur(function(){
+			if($(this).val() == ''){
+				$(this).val(text).css('color','#aaa');
+			}
+		});
+
+		return this;
+	}
+});
+
