4230c7ef4b2a420de553423627ba21ca0d27d99b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-05-19 17:13:05 -0300

    Modificado o plugin de placeholder para funcionar apenas se o campo for vazio

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,18 +1,20 @@
 jQuery.fn.extend({
 	placeholder: function (text) {
-		$(this).val(text).css('color','#aaa')
-		.focus(function(){
-			if($(this).val() == text){
-				$(this).css('color','').val('');
-			}
-		})
-		.blur(function(){
-			if($(this).val() == ''){
-				$(this).val(text).css('color','#aaa');
-			}
-		});
+		if ($(this).val() == '') {
+			$(this).val(text).css('color', '#aaa')
+			.focus(function () {
+				if($(this).val() == text){
+					$(this).css('color','').val('');
+				}
+			})
+			.blur(function () {
+				if ($(this).val() == ''){
+					$(this).val(text).css('color','#aaa');
+				}
+			});
 
-		return this;
+			return this;
+		}
 	}
 });
 
