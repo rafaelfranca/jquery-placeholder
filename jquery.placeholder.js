72df19929804cb8829644b59724041f5edac7b32
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-07-21 17:19:40 -0300

    Corrigido o método para adicionar placeholder mesmo que se existir conteúdo no campo

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,20 +1,23 @@
 jQuery.fn.extend({
 	placeholder: function (text) {
-		if ($(this).val() == '') {
-			$(this).val(text).css('color', '#aaa')
-			.focus(function () {
-				if($(this).val() == text){
-					$(this).css('color','').val('');
-				}
-			})
-			.blur(function () {
-				if ($(this).val() == ''){
-					$(this).val(text).css('color','#aaa');
-				}
-			});
+		$(this).val(text).css('color', '#aaa')
+		.focus(function () {
+			if($(this).val() == text){
+				$(this).css('color','').val('');
+			}
+		})
+		.blur(function () {
+			if ($(this).val() == ''){
+				$(this).val(text).css('color','#aaa');
+			}
+		});
 
-			return this;
-		}
+		return this;
 	}
 });
 
+$(function() {
+	$('input[placeholder]').each(function() {
+		$(this).placeholder($(this).attr('placeholder'));
+	});
+});
