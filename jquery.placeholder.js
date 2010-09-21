ccd76d8e21eb2d991e0e53b1ad136bb1b4504802
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-09-21 10:14:49 -0300

    Retirado alguns warnings

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,14 +1,12 @@
 jQuery.fn.extend({
 	placeholder: function (text) {
-		if ((text == $(this).val()) || ($(this).val() == "")) {
-			$(this).val(text).css('color', '#aaa')
-			.focus(function () {
+		if ((text === $(this).val()) || ($(this).val() === "")) {
+			$(this).val(text).css('color', '#aaa').focus(function () {
 				if($(this).val() == text){
 					$(this).css('color','').val('');
 				}
-			})
-			.blur(function () {
-				if ($(this).val() == ''){
+			}).blur(function () {
+				if ($(this).val() === ''){
 					$(this).val(text).css('color','#aaa');
 				}
 			});
